6907be4fe7cbbeb3dce163ecb8bc2e0ca22e8b4b
aksel26 <kevinxkim2023@gmail.com>
2022-09-19 11:26:43 +0900

    ADD : TypographyComponent

--- src/components/UI/atoms/Typo.jsx
+++ src/components/UI/atoms/Typo.jsx
@@ -1,5 +1,11 @@
+import { Typography } from "@mui/material";
 import React from "react";
 
-export default function Typo() {
-  return <div>asdfsafd</div>;
+export default function Typo(props) {
+  console.log("props: ", props);
+  return (
+    <Typography variant={props.variant} {...props}>
+      {props.text}
+    </Typography>
+  );
 }
--- src/stories/Typo.stories.jsx
+++ src/stories/Typo.stories.jsx
@@ -3,16 +3,37 @@ import Typo from "../components/UI/atoms/Typo";
 export default {
   title: "atoms/Text",
   component: Typo,
-  args: {
-    type: "primary",
+  argTypes: {
     text: "아놔",
+    variant: {
+      options: [
+        "body1",
+        "body2",
+        "button",
+        "caption",
+        "h1",
+        "h2",
+        "h3",
+        "h4",
+        "h5",
+        "h6",
+        "inherit",
+        "overline",
+        "subtitle1",
+        "subtitle2",
+      ],
+      control: { type: "radio" },
+    },
+    align: {
+      options: [, "center", "inherit", "justify", "left", "right"],
+      control: { type: "radio" },
+    },
   },
 };
 
-export const Default = (args) => <Typo {...args} />;
+const Template = (args) => <Typo {...args} />;
 
-// export const Default = Template.bind({});
-// Default.args = {
-//   type: "primary",
-//   text: "아놔",
-// };
+export const Text = Template.bind({});
+Text.args = {
+  text: "아놔",
+};
